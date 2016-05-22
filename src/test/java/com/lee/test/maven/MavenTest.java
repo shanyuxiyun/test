9ecb7d9aa2d249886f148cca58db26f8aca95d72
shanyuxiyun <lidong198609@hotmail.com>
2016-05-22 10:53:32 +0800

    maven update

---
+++ src/test/java/com/lee/test/maven/MavenTest.java
@@ -0,0 +1,13 @@
+package com.lee.test.maven;
+
+public class MavenTest {
+
+	// mvn help:help
+	// mvn help:system
+
+	// mvn dependency:sources
+	// mvn dependency:resolve -Dclassifier=javadoc
+
+	// mvn dependency:get
+	// -Dartifact=org.apache.maven.wagon:wagon-provider-api:1.0:jar:sources
+}
